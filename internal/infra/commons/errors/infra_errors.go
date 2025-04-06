9c0a9e2874f63bab4df993b997c4b4f6db42f057
Andrei Santos <andrei.as3@hotmail.com>
2025-04-06 03:11:58 -0300

    feat: implements infra errors uow

---
+++ internal/infra/commons/errors/infra_errors.go
@@ -0,0 +1,64 @@
+package errors
+
+import (
+	domain_errors "github.com/andreis3/users-ms/internal/domain/errors"
+)
+
+const (
+	ErrInternalProcessing domain_errors.ErrorCode = "IMF-500"
+)
+
+const (
+	InternalServerError        = "internal server error"
+	ServerErrorFriendlyMessage = "Server suffered an internal error"
+)
+
+func ErrorTransactionAlreadyExists() *domain_errors.AppErrors {
+	return &domain_errors.AppErrors{
+		Code:            ErrInternalProcessing,
+		Errors:          []string{"Transaction already exists"},
+		Cause:           InternalServerError,
+		OriginFunc:      "UnitOfWork.Do",
+		FriendlyMessage: ServerErrorFriendlyMessage,
+	}
+}
+
+func ErrorOpeningTransaction(err error) *domain_errors.AppErrors {
+	return &domain_errors.AppErrors{
+		Code:            ErrInternalProcessing,
+		Errors:          []string{err.Error()},
+		Cause:           InternalServerError,
+		OriginFunc:      "UnitOfWork.Do",
+		FriendlyMessage: ServerErrorFriendlyMessage,
+	}
+}
+
+func ErrorRollBackTransactionEmpty() *domain_errors.AppErrors {
+	return &domain_errors.AppErrors{
+		Code:            ErrInternalProcessing,
+		Errors:          []string{"Transaction is empty"},
+		Cause:           InternalServerError,
+		OriginFunc:      "UnitOfWork.Rollback",
+		FriendlyMessage: ServerErrorFriendlyMessage,
+	}
+}
+
+func ErrorExecuteRollback(err error) *domain_errors.AppErrors {
+	return &domain_errors.AppErrors{
+		Code:            ErrInternalProcessing,
+		Errors:          []string{err.Error()},
+		Cause:           InternalServerError,
+		OriginFunc:      "UnitOfWork.Rollback",
+		FriendlyMessage: ServerErrorFriendlyMessage,
+	}
+}
+
+func ErrorCommitOrRollback(err error) *domain_errors.AppErrors {
+	return &domain_errors.AppErrors{
+		Code:            ErrInternalProcessing,
+		Errors:          []string{err.Error()},
+		Cause:           InternalServerError,
+		OriginFunc:      "UnitOfWork.CommitOrRollback",
+		FriendlyMessage: ServerErrorFriendlyMessage,
+	}
+}
