5905a38921b3af2980af15b96be24b2e90874301
Andrei Santos <andrei.as3@hotmail.com>
2025-07-09 23:13:15 -0300

    refactor: adjust name vo

--- internal/app/mapper/to_login.go
+++ internal/app/mapper/to_login.go
@@ -4,8 +4,8 @@ import (
 	"github.com/andreis3/customers-ms/internal/app/dto"
 )
 
-func TokenOutput(token string, expiresAt int64) dto.LoginOutput {
-	return dto.LoginOutput{
+func TokenOutput(token string, expiresAt int64) *dto.LoginOutput {
+	return &dto.LoginOutput{
 		Token:     token,
 		ExpiresAt: expiresAt,
 	}
